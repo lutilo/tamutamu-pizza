5e96b5640880938fdc35956550975414bb834e5d
brayooh <brianndichu.bn@gmail.com>
2020-05-24 23:38:10 +0300

    adds function to submit pizza type info

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function Pizza(size,topping,crust, name){
+  this.size = size;
+  this.topping = topping;
+  this.crust = crust;
+  this.name = name;
+}
+
+
+
+$(document).ready(function(){
+  $("form.myform").submit(function(event){
+    event.preventDefault();
+    var sizep = parseInt($("#sizes option:selected").val());
+    var topp = parseInt($(".toppings input:checked").val());
+    var crustp = parseInt($(".crusts input:checked").val());
+    var deliveryp = parseInt($(".delivery input:checked").val());
+    var quantity = parseInt($(".quantity").val());
+    var total = parseInt(sizep + topp + crustp + deliveryp);
+    var grandTotal = parseInt(total*quantity);
+
